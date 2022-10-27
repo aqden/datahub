7729ae5dafba919141395d851ddfe00c97ea18f2
junjie <neojunjie87@gmail.com>
2022-10-27 16:15:02 +0700

    should not import packages with .*

--- datahub-graphql-core/src/main/java/com/linkedin/datahub/graphql/types/timeline/mappers/DatasetChangeEventsMapper.java
+++ datahub-graphql-core/src/main/java/com/linkedin/datahub/graphql/types/timeline/mappers/DatasetChangeEventsMapper.java
@@ -1,9 +1,12 @@
 package com.linkedin.datahub.graphql.types.timeline.mappers;
 
-import com.linkedin.datahub.graphql.generated.*;
+import com.linkedin.datahub.graphql.generated.ChangeEvent;
+import com.linkedin.datahub.graphql.generated.GetDatasetChangeEventsResult;
 import com.linkedin.metadata.timeline.data.ChangeTransaction;
 import lombok.extern.slf4j.Slf4j;
-import java.util.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 public class DatasetChangeEventsMapper {
