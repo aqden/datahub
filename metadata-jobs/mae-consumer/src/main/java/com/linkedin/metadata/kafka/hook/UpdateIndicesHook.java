ad812ef8d79d47a13c015f2b798f0e54ffc97661
neojunjie <neojunjie87@gmail.com>
2024-02-21 10:07:32 +0800

    Revert "Revert "Revert "add missing code to UpdateIndicesService.java and revert last commit"""
    This reverts commit 6e2b2e9cd68912770b931afff3eda4ad16b4a729.

--- metadata-jobs/mae-consumer/src/main/java/com/linkedin/metadata/kafka/hook/UpdateIndicesHook.java
+++ metadata-jobs/mae-consumer/src/main/java/com/linkedin/metadata/kafka/hook/UpdateIndicesHook.java
@@ -27,6 +27,9 @@ public class UpdateIndicesHook implements MetadataChangeLogHook {
   protected final UpdateIndicesService _updateIndicesService;
   private final boolean _isEnabled;
 
+  @Value("${featureFlags.preProcessHooks.uiEnabled:true}")
+  private boolean isUiEnabled;
+
   public UpdateIndicesHook(
       UpdateIndicesService updateIndicesService,
       @Nonnull @Value("${updateIndices.enabled:true}") Boolean isEnabled) {
@@ -43,10 +46,11 @@ public class UpdateIndicesHook implements MetadataChangeLogHook {
   public void invoke(@Nonnull final MetadataChangeLog event) {
     if (event.getSystemMetadata() != null) {
       if (event.getSystemMetadata().getProperties() != null) {
-        if (UI_SOURCE.equals(event.getSystemMetadata().getProperties().get(APP_SOURCE))) {
+        if (UI_SOURCE.equals(event.getSystemMetadata().getProperties().get(APP_SOURCE)) && isUiEnabled ) {
           // If coming from the UI, we pre-process the Update Indices hook as a fast path to avoid Kafka lag
           return;
         }
+        _updateIndicesService.handleChangeEvent(event);
       }
     }
   }
